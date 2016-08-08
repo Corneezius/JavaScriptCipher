92310d7336031071d04d744f00c78ca337161e42
Lisa MacCarrigan <lisa@>
2016-08-08 15:48:00 -0400

    add fourth js function, use jQuery to create two clickable headers

--- js/script.js
+++ js/script.js
@@ -1,25 +1,52 @@
 // var sentence = prompt("Enter a sentence.");
 // console.log(sentence);
 
-// var capitalize = function(sentence) {
-//   sentence.trim();
-//   var end = sentence.length - 1;
-//   var firstLetter = sentence.substr(0,1).toUpperCase();
-//   var lastLetter = sentence.substr(sentence.length - 1).toUpperCase();
-//   var newSentence = firstLetter + sentence.substr(1,sentence.length-2) + lastLetter;
-//   return newSentence;
-// }
-//
+var capitalize = function(sentence) {
+  sentence.trim();
+  var end = sentence.length - 1;
+  var firstLetter = sentence.substr(0,1).toUpperCase();
+  var lastLetter = sentence.substr(sentence.length - 1).toUpperCase();
+  var newSentence = firstLetter + lastLetter;
+  return newSentence;
+}
+
 // console.log(capitalize(sentence));
 
 var reverse = function(sentence) {
     sentence.trim();
     // removes extra spaces
-    var firstLetter = sentence.substr(0,1);
-    var secondLetter = sentence.substr(1,1);
-    return secondLetter + firstLetter + sentence.substr(2);
+    var firstLetter = sentence.charAt(0);
+    var secondLetter = sentence.charAt(1);
+    return secondLetter + firstLetter;
     //  takes first two character of argument and reverses them. also stores as variable "firstreverse"
 
 }
-var backwards = prompt("Enter a word and we're going to mess it up");
-console.log(reverse(backwards));
+// var backwards = prompt("Enter a word and we're going to mess it up");
+// console.log(reverse(backwards));
+
+// Third function
+
+var both = function(sentence){
+  sentence.trim();
+  return sentence + reverse(capitalize(sentence));
+
+}
+
+//Fourth function
+var middleChar = function(sentence) {
+  var halfLength = parseInt(sentence.length / 2);
+  return sentence.charAt(halfLength) + sentence;
+}
+//
+// var sentence = prompt("Enter a sentence");
+// console.log(middleChar(both(sentence)).split("").reverse().join(""));
+
+jQuery(document).ready(function() {
+  jQuery("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  jQuery("h2").click(function() {
+    alert("This is a header, too");
+  });
+});
